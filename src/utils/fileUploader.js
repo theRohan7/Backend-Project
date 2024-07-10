2a1cc45f1c987546f5667761cf8de35bc4b1ecd6
Rohan sahu <therohansahu7@gmail.com>
2024-07-10 15:32:00 +0530

    fileUploading with cloudinary

---
+++ src/utils/fileUploader.js
@@ -0,0 +1,33 @@
+//    CLOUDINARY
+
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';                               //FileSystem    default package in Node JS
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET_KEY 
+});
+
+
+const UploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return console.error('Could not find File path :( ')
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:'auto'
+        })
+        //file has been uploaded sucessfully
+        console.log("file is uploaded on Cloudinary", response.url);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)  //Removes the locally saved temmporary file as the uploading operation failed.
+        return null;
+
+        
+    }
+}
+
+
+export {UploadOnCloudinary}
