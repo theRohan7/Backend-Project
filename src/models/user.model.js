98e0e9a6fc972f917ef96efd5819fe0b33f48d06
Rohan sahu <therohansahu7@gmail.com>
2024-07-10 14:45:23 +0530

    added some methods into models and JWT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
-import mongoose ,  {Schema, Types} from "mongoose";
+import mongoose ,  {Schema} from "mongoose";
+import jwt  from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+
 
 
 const userSchema = new Schema(
@@ -18,7 +21,7 @@ const userSchema = new Schema(
         lowercase: true,
         trim: true,
     },
-    fullname:{
+    fullName:{
         type: String,
         required: true,
         lowercase: true,
@@ -52,5 +55,45 @@ const userSchema = new Schema(
     }
 )
 
+//encrypt password before saving it to datbase. 
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Check the encrypted password with user given password
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
